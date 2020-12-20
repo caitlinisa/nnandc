95df02009dcef51574e4968b2372a39a32ccb336
caitlin <hello@hello>
2020-12-20 19:42:21 +0100

    extra stuff done

--- rosenblatt.py
+++ rosenblatt.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+
 def generate_data(Pnum,Ndim):
     data = np.random.normal(0, 1, size=(Ndim, Pnum))
     #print (data.shape)
@@ -13,28 +14,20 @@ def generate_labels(Pnum):
     #print (labels.shape)
     return labels
 
-def rosenblatt_training(data,labels,maxEpochs):
+def rosenblatt_training(data,labels,maxEpochs,c):
     n,p = data.shape
     weights = np.zeros((n,1))
     succeed = 0
     for t in range(maxEpochs):
         for idx in range(p):
             data_ex = data[:,idx]
-            #print (data.shape)
             labels_ex = labels[idx,:]
             data_ex = np.reshape(data_ex,(n,1))
             labels_ex = np.reshape(labels_ex,(1,1))
-            #print(data_ex.shape)
-            #print(labels_ex.shape)
-            #print (labels.shape)
             weight_vector = np.squeeze(np.asarray(weights))
             data_vector = np.squeeze(np.asarray(data_ex))
-            #print(weight_vector)
-            #print (data_vector.shape)
             E = ((np.dot(weight_vector , data_ex) * labels_ex)[0])[0]
-            #print(E)
-            #exit(0)
-            if E > 0:
+            if E > c:
                 weights = weights
                 succeed += 1
             else:
@@ -49,34 +42,83 @@ def rosenblatt_training(data,labels,maxEpochs):
         succeed = 0 
             
 
-            
-succeeded = 0
-failed = 0 
-ratio = []
-num_examples = [15,20,25,30,35,40,45,50,55,60]
-n = 20 
-totalruns = 50
-maxEpochs = 100  
-for ex in range(len(num_examples)):     
-    for idx in range(totalruns):
-        data = generate_data(num_examples[ex],n)
-        labels = generate_labels(num_examples[ex])
-        success = rosenblatt_training(data,labels,100)
-        if success:
-            succeeded += 1
-        else:
-            failed += 1
-    fraction = succeeded/totalruns
-    ratio.append(fraction)
+def compareN():        
     succeeded = 0
-    failed = 0
+    failed = 0 
+    ratio = []
+    #num_examples = [15,20,25,30,35,40,45,50,55,60]
+    alpha = [0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0]
+    alpha = [1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5]
+    n = [20,60,100] 
+    xaxis  = [0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0]
+    totalruns = 50
+    maxEpochs = 100
+    labels = ['N = 20', 'N = 60','N = 100'] 
+
+    for N in n:
+        #num_examples = N * alpha
+        for ex in range(len(alpha)):     
+            for idx in range(totalruns):
+                #print(alpha[ex]*N)
+                data = generate_data(int(alpha[ex]*N),N)
+                labels = generate_labels(int(alpha[ex]*N))
+                success = rosenblatt_training(data,labels,100)
+                if success:
+                    succeeded += 1
+                else:
+                    failed += 1
+            fraction = succeeded/totalruns
+            ratio.append(fraction)
+            succeeded = 0
+            failed = 0
+        plt.plot(alpha,ratio)
+        ratio = []
+
+    plt.legend(['N = 20', 'N = 60','N = 100'] ,loc = "upper right")
+    plt.title("Fraction of successful runs against the value of alpha")
+    plt.xlabel('alpha')
+    plt.ylabel('Fraction of successful runs')
+    plt.show()
+
+def compareC():
+    succeeded = 0
+    failed = 0 
+    ratio = []
+    N = 100
+    #num_examples = [15,20,25,30,35,40,45,50,55,60]
+    alpha = [0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0]
+    alpha = [1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5]
+    c = [0,0.1,0.2] 
+    xaxis  = [0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0]
+    totalruns = 50
+    maxEpochs = 100
+    labels = ['c = 0', 'c = 0.1','c = 0.2'] 
+
+    for C in c:
+        #num_examples = N * alpha
+        for ex in range(len(alpha)):     
+            for idx in range(totalruns):
+                #print(alpha[ex]*N)
+                data = generate_data(int(alpha[ex]*N),N)
+                labels = generate_labels(int(alpha[ex]*N))
+                success = rosenblatt_training(data,labels,100,C)
+                if success:
+                    succeeded += 1
+                else:
+                    failed += 1
+            fraction = succeeded/totalruns
+            ratio.append(fraction)
+            succeeded = 0
+            failed = 0
+        plt.plot(alpha,ratio)
+        ratio = []
+
+    plt.legend(['c = 0', 'c = 0.1','c = 0.2']  ,loc = "upper right")
+    plt.title("Fraction of successful runs against the value of alpha")
+    plt.xlabel('alpha')
+    plt.ylabel('Fraction of successful runs')
+    plt.show()
 
 
-xaxis  = [0.75,1.0,1.25,1.5,1.75,2.0,2.25,2.5,2.75,3.0]
-plt.plot(xaxis,ratio)
-plt.title("Fraction of successful runs against the value of alpha")
-plt.xlabel('alpha')
-plt.ylabel('Fraction of successful runs')
-plt.show()
 
-
+compareC()
